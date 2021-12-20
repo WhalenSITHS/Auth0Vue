3d686c63856041a8c31c30356462513e6f5cbadf
mr. whalen <michael.whalen@sitechhs.com>
2021-12-20 09:34:49 -0500

    dfg

--- hello-auth/src/views/ExternalApi.vue
+++ hello-auth/src/views/ExternalApi.vue
@@ -18,16 +18,35 @@ export default {
   methods: {
     async callApi() {
       // Get the access token from the auth wrapper
-      const token = await this.$auth.getTokenSilently();
+      //const token = await this.$auth.getTokenSilently();
+
+      try {
+        const token = await this.$auth.getTokenSilently();
+        const response = await fetch("http://localhost:3000/authorized", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+        this.apiMessage = data;
+      } catch (error) {
+        console.log(error);
+      }
 
       // Use Axios to make a call to the API
-      const { data } = await axios.get("localhost:3000/authorized", {
-        headers: {
-          Authorization: `Bearer ${token}`, // send the access token through the 'Authorization' header
-        },
-      });
+      /*  const { data } = await axios
+        .get("localhost:3000/authorized", {
+          headers: {
+            Authorization: `Bearer ${token}`, // send the access token through the 'Authorization' header
+          },
+        })
+        .then(console.log(data))
+        .catch(function (error) {
+          console.log(error);
+        });
 
-      this.apiMessage = data;
+      this.apiMessage = data; */
     },
   },
 };
